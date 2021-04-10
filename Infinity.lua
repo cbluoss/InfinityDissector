512ce5c82517e171287dc93f5c0b1990e74b444e
yourcoke <deinkoks@gmail.com>
2021-04-10 23:46:06 +0200

    refactoring

--- Infinity.lua
+++ Infinity.lua
@@ -1,21 +1,62 @@
 infinity_protocol = Proto("Infinity",  "Infinity Network Protocol")
+infinity_protocol.fields = {}
 
-message_timestamp = ProtoField.int32("infinity.message_timestamp", "messageTimestamp", base.DEC)
-message_length = ProtoField.int16("infinity.message_length", "messageLength", base.DEC)
-message_device = ProtoField.string("infinity.device", "device", base.ASCII)
-message_alert = ProtoField.string("infinity.alert", "alert", base.ASCII)
+-- What we know about the protocol so far:
+-- * a 18(?) Byte header with a message_timestamp followed for
+-- * 660 Byte broadcasts used for alarm and status messages e.g. "ECG electrodes removed"
+-- * >1000 Byte broadcast depending on the current device configuration send every second  with all current datapoints
+--   * 36 Bytes for each of most datapoints (including lower/upper alarm thresholds, the measured value)
+
+
+-- Common packet fields:
+packet_length = ProtoField.int16("infinity.packet.length", "Packet length", base.DEC)
+infinity_protocol.fields[#infinity_protocol.fields+1]=packet_length
+packet_timestamp = ProtoField.int32("infinity.packet.timestamp", "Packet timestamp", base.DEC)  -- 0x14,4
+infinity_protocol.fields[#infinity_protocol.fields+1]=packet_timestamp
+
+-- Message packet fields:
+is_message = ProtoField.bool("infinity.packet.message", "Is Message")
+infinity_protocol.fields[#infinity_protocol.fields+1]=is_message
+message_device = ProtoField.string("infinity.message.device", "Device", base.ASCII)             -- 0x60,16
+infinity_protocol.fields[#infinity_protocol.fields+1]=message_device
+message_alert = ProtoField.string("infinity.message.alert", "Alert", base.ASCII)                -- 0xAE,32
+infinity_protocol.fields[#infinity_protocol.fields+1]=message_alert
+
+patient_name = ProtoField.string("infinity.patient.name", "Patient Name", base.ASCII)           -- 0x24,48
+infinity_protocol.fields[#infinity_protocol.fields+1]=patient_name
+
+-- Data packet fields:
+-- (basic) Alarm tresholds are usually -2/+2 bytes  relative to the measured value
+is_data = ProtoField.bool("infinity.packet.data", "Is Data")
+infinity_protocol.fields[#infinity_protocol.fields+1]=is_data
+
+respiratory_rate = ProtoField.int8("infinity.data.respiratory_rate", "Respiratory Rate", base.DEC)  -- 0x00DB
+infinity_protocol.fields[#infinity_protocol.fields+1]=respiratory_rate
+
+spo2 = ProtoField.int8("infinity.data.spo2", "sPO2", base.DEC)                                      -- 0x01FF
+infinity_protocol.fields[#infinity_protocol.fields+1]=spo2
+spo2_pulse = ProtoField.int8("infinity.data.spo2.pulse", "sPO2 Pulse", base.DEC)                    -- 0x0123
+infinity_protocol.fields[#infinity_protocol.fields+1]=spo2_pulse
+
+nibp_sys = ProtoField.int8("infinity.data.nibp.sys", "NIBP (Systolic)", base.DEC)                   -- 0x0147
+infinity_protocol.fields[#infinity_protocol.fields+1]=nibp_sys
+nibp_dia = ProtoField.int8("infinity.data.nibp.dia", "NIBP (Diastolic)", base.DEC)                  -- 0x016B
+infinity_protocol.fields[#infinity_protocol.fields+1]=nibp_dia
+nibp_map = ProtoField.int8("infinity.data.nibp.map", "NIBP (MAP)", base.DEC)                        -- 0x018F
+infinity_protocol.fields[#infinity_protocol.fields+1]=nibp_map
 
 RRsys = ProtoField.int16("infinity.RRsys", "RRsys", base.DEC)
 RRdia = ProtoField.int16("infinity.RRdia", "RRdia", base.DEC)
 RRmad = ProtoField.int16("infinity.RRmad", "RRmad", base.DEC)
+NIBPsys = ProtoField.string("infinity.nibp.sys", "NIBP Systolic", base.ASCII)
 
 sPO2 = ProtoField.int16("infinity.sPO2", "sPO2", base.DEC)
 sPO2_P = ProtoField.int16("infinity.sPO2_P", "sPO2 Puls", base.DEC)
 
-PatientName = ProtoField.string("infinity.PatientName", "Patient Name", base.ASCII)
-infinity_protocol.fields = {message_timestamp, message_length, message_device, message_alert, RRsys, RRdia, RRmad, sPO2, sPO2_P, PatientName}
+infinity_protocol.fields = {RRsys, RRdia, RRmad, sPO2, sPO2_P, NIBPsys }
 
 function infinity_protocol.dissector(buffer, pinfo, tree)
+
   length = buffer:len()
   if length == 0 then return end
 
@@ -23,30 +64,28 @@ function infinity_protocol.dissector(buffer, pinfo, tree)
 
   local subtree = tree:add(infinity_protocol, buffer(), "infinity Protocol Data")
 
-  subtree:add(message_timestamp, buffer(0x14,4))
-  subtree:add(message_length, length)
+  subtree:add(packet_timestamp, buffer(0x14,4))
+  subtree:add(packet_length, length)
 
   if length == 660 then 
-  	-- local device_str = ""
-  	-- local device = buffer:bytes(0x61,16)
-  	-- for i, _ in ipairs(device) do
-  	-- 	device_str = device_str .. device[i]
-  	-- end
-
+    subtree:add(is_message, true)
   	subtree:add(message_device, buffer:range(0x60,16):ustring())
   	subtree:add(message_alert, buffer:range(0xAE,32):ustring())
-  	subtree:add(PatientName, buffer:range(0x24,48):ustring())
+  	subtree:add(patient_name, buffer:range(0x24,48):ustring())
 
   end
 
 
   if length > 1000 then 
-  	subtree:add(RRsys, buffer(0x0147,1)) 
-  	subtree:add(RRdia, buffer(0x016B,1)) 
-  	subtree:add(RRmad, buffer(0x018F,1)) 
+    subtree:add(is_data, true)
+  	subtree:add(respiratory_rate, buffer(0x00DB,1)) 
+    subtree:add(spo2, buffer(0x01FF,1)) 
+    subtree:add(spo2_pulse, buffer(0x0123,1)) 
+    subtree:add(nibp_sys, buffer(0x0147,1)) 
+    subtree:add(nibp_dia, buffer(0x016B,1)) 
+    subtree:add(nibp_map, buffer(0x018F,1)) 
 
-   	subtree:add(sPO2, buffer(0x00FF,1)) 
-  	subtree:add(sPO2_P, buffer(0x0123,1)) 
+
   end
 
 
